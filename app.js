1ceca03ca97e10c5be24606a2238b58e240f09c6
Suissa <jnascimento@gmail.com>
2014-07-20 19:50:53 -0300

    Buscanbdo cervejas de uma API externa

--- app.js
+++ app.js
@@ -49,36 +49,22 @@ if (env === 'production') {
 // serve index and view partials
 app.get('/', routes.index);
 app.get('/partials/preload', function(req, res){
-  var beers = [{"alcohol":4.5,"price":3,"_id":"53c6f9a66c2303373b944322","__v":0,"created":"2014-07-16T22:16:06.847Z","category":"pilsen","description":"Mijo de Rato","name":"Skol"},{"price":99,"alcohol":8,"_id":"53c9a37c353837acbf84951b","__v":0,"created":"2014-07-18T22:45:16.571Z","category":"","description":"","name":"TESTE"},{"__v":0,"_id":"53c6f51fae30b8d7392a8ce2","alcohol":4.5,"price":3,"created":"2014-07-16T21:56:47.946Z","category":"pilsen","description":"Mijo de Rato","name":"Skolzinha"}];
-  
   var url = 'http://api.openbeerdatabase.com/v1/beers.json';
-
-
-  // var options = {
-  //   host: 'api.openbeerdatabase.com',
-  //   port: 80,
-  //     path: '/v1/beers.json',
-  // };
-
-  http.get(url, function(res){
-    res.setEncoding('utf8');
-    res.on('data', function(chunk){
-        console.log(chunk);
+  http.get(url, function(response){
+    response.setEncoding('utf8');
+    var body = '';
+    response.on('data', function(chunk){
+        // console.log(chunk);
+        body += chunk;
+        // res.render('partials/preload', {beers: chunk});
+    });
+    response.on('end', function () {
+      var bodyJSON = JSON.parse(body)
+      console.log('body', bodyJSON);
+      res.render('partials/preload', {beers: bodyJSON.beers});
     });
 
   });
-
-  // http.get(options, function(res) {
-  //   console.log("Got response: " + res.statusCode);
-
-  //   res.on("data", function(chunk) {
-  //     console.log("BODY: " + chunk);
-  //   });
-  // }).on('error', function(e) {
-  //   console.log("Got error: " + e.message);
-  // });
-
-  res.render('partials/preload', {beers: beers});
 
 });
 app.get('/partials/:name', routes.partials);
